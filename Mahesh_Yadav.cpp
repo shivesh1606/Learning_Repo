c35456f64f6a070d039b3da8f82b2403185328a2
Mahesh212121 <96724282+Mahesh212121@users.noreply.github.com>
2022-12-19 13:30:25 +0530

    Create Mahesh_Yadav.cpp

---
+++ Mahesh_Yadav.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+  int n,r,reverse=0,temp;    
+  cout<<"Enter the Number=";    
+  cin>>n;    
+ temp=n;    
+ while(n>0)    
+{    
+ r=n%10;    
+ reverse=(reverse*10)+r;    
+ n=n/10;    
+}    
+if(temp==reverse)    
+cout<<"Number is Palindrome.";    
+else    
+cout<<"Number is not Palindrome.";   
+  return 0;  
+}  
